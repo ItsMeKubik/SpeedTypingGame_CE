672a829e5addb25fb4d93d23a8a57d82b19b5984
ItsMeKubik <typekjmenemkubik@gmail.com>
2025-04-22 13:43:50 +0200

    Added README.md

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# SpeedTypingGame_CE
-Console edition of my SpeedTypingGame
+# Speed typing game
+
+A console speed typing game made as my uni project.
+
+### Features
+- **Two game modes!**
+  - Words per minute mode (Counts your words per minute)
+  - Vs opponent mode (A race against fictional opponent, who has more words per minute wins,
+   can you beat the opponent?)
+- **Multiple difficulty levels!**
+- **No GUI, runs directly in your system console!**
+
+## Installation
+1. **Clone the repository**
+    - Navigate where you want to install the game
+    ```bash
+    git clone
+    cd SpeedTypingGame_CE
+
+2. **Install dependencies**
+    ```bash
+   pip install requirements.txt
+   
+3. **Run the game**
+    ```bash
+   python game.py
+   
+## Tests
+- Tests are handled using pytest
+    ```bash
+    pytest
+
+## License
+Author: Jakub Prusenovský\
+Code is published under [MIT](https://opensource.org/license/mit) license.
--- game.py
+++ game.py
@@ -6,77 +6,69 @@ import sys
 
 init()
 
+
 def clear_terminal():
+
     """
     Clears the terminal screen.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def countdown(seconds=3):
-    """
-    Counts down the given number of seconds.
-    """
-    for i in range(seconds,0,-1):
-        print(Fore.YELLOW + f"Starting in {i}..." + Style.RESET_ALL)
-        time.sleep(1)
-    clear_terminal()
-
-def simulate_opponent_progress(opponent_wpm : int):
-    """
-    Simulates the opponent progress.
-
-    Parameters
-    ----------
-    opponent_wpm: int
-
-    At the end prints the opponent wpm the player has to beat.
-    """
-    for i in range(4):
-        print(Fore.YELLOW + "Opponent is typing...")
-        time.sleep(1)
-    print(f"Opponent has finished typing, his wpm is {opponent_wpm} WPM. Can you beat it?")
-
-def count_words_per_minute(time_start,mode, correct_words : int):
-    """
-    Counts the words per minute player has.
-
-    Parameters
-    ----------
-    time_start: int - Timer to count the start of the game in minutes.
-    mode: int - The game mode player plays.
-    correct_words: int - Number of correct words.
-
-
-    At the end, based on what mode the player is playing, either prints player's typing speed or returns words per minute.
-    """
-    end_time = (time.time() / 60)
-    final_time = end_time - time_start
-    words_per_minute = (correct_words / final_time)
-    print(Fore.YELLOW + "GAME OVER! Counting words per minute..." + Style.RESET_ALL)
-    time.sleep(2)
-    clear_terminal()
-    if mode == 1:
-        print(Fore.YELLOW + "Your typing speed is: " + Fore.GREEN + str(round(words_per_minute,1)) + Fore.YELLOW + " Words per minute" + Style.RESET_ALL)
-    elif mode == 2:
-        return round(words_per_minute,1)
 
 def load_words():
     """
-    Loads words the game will use from file then randomly shuffles them.
+    Loads words the game will use from file .
+
+    Return the words in a randomly shuffled array.
     """
     with open("words_to_play.txt") as f:
         words = f.read().split(",")
     random.shuffle(words)
     return words
 
+
+def countdown(seconds=3):
+    """
+    Counts down the given number of seconds.
+    """
+    for i in range(seconds, 0, -1):
+        print(Fore.YELLOW + f"Starting in {i}..." + Style.RESET_ALL)
+        time.sleep(1)
+
+    clear_terminal()
+
+
 def words_per_minute_mode():
     """
     Words per minute mode.
+
+    Prompts the player to press ENTER to start.
+    Then starts the countdown and calls game master function `gm()` with
+    parameter '1' = words per minute mode
     """
     input(Style.RESET_ALL + "Press ENTER to start...")
     clear_terminal()
     countdown()
     gm(1)
+
+
+def vs_opponent():
+    """
+    Versus opponent mode.
+
+    Prompts the player to press ENTER to start.
+    Then starts the countdown and calls game master function `gm()` with
+    parameter '2' = vs opponent mode
+    """
+    clear_terminal()
+    opponent_wpm = difficulty_chooser()
+    clear_terminal()
+    simulate_opponent_progress(opponent_wpm)
+    input(Style.RESET_ALL + "Press ENTER to start...")
+    clear_terminal()
+    countdown()
+    gm(2, opponent_wpm)
+
 
 def difficulty_chooser():
     """
@@ -91,53 +83,84 @@ def difficulty_chooser():
     - HARD: Number between 40 and 50
 
     Any invalid input sets the game difficulty to medium
+    At the end returns opponent words per minute.
     """
     print(Fore.YELLOW + "Please select difficulty (1,2,3)" + Style.RESET_ALL)
     print("1.Easy\n2.Medium\n3.Hard")
     difficulty = input()
     if difficulty.isdigit() and (difficulty == "1" or difficulty == "2" or difficulty == "3"):
         if difficulty == "1":
-            opponent_wpm = random.randint(10,24)
+            opponent_wpm = random.randint(10, 24)
         elif difficulty == "2":
-            opponent_wpm = random.randint(25,39)
+            opponent_wpm = random.randint(25, 39)
         elif difficulty == "3":
-            opponent_wpm = random.randint(40,50)
+            opponent_wpm = random.randint(40, 50)
     else:
         print(Fore.RED + "INVALID INPUT! Defaulting to medium difficulty!" + Style.RESET_ALL)
         time.sleep(2)
         clear_terminal()
-        opponent_wpm = random.randint(25,39)
+        opponent_wpm = random.randint(25, 39)
     return opponent_wpm
 
-def vs_opponent():
-    """
-    Versus opponent mode.
-    """
-    clear_terminal()
-    opponent_wpm = difficulty_chooser()
-    clear_terminal()
-    simulate_opponent_progress(opponent_wpm)
-    input(Style.RESET_ALL + "Press ENTER to start...")
-    clear_terminal()
-    countdown()
-    gm(2,opponent_wpm)
 
-def mode_selector(mode : int):
+def simulate_opponent_progress(opponent_wpm: int):
+    """
+    Simulates the opponent progress.
+
+    Parameters
+    ----------
+    - opponent_wpm: int
+
+    At the end prints the opponent wpm the player has to beat.
+    """
+    for i in range(4):
+        print(Fore.YELLOW + "Opponent is typing...")
+        time.sleep(1)
+    print(f"Opponent has finished typing, his wpm is {opponent_wpm} WPM. Can you beat it?")
+
+
+def count_words_per_minute(time_start, mode, correct_words: int):
+    """
+    Counts the words per minute player has.
+
+    Parameters
+    ----------
+    - time_start: int -- Timer to count the start of the game in minutes.
+    - mode: int -- The game mode player plays.
+    - correct_words: int -- Number of correct words.
+
+
+    At the end, based on what mode the player is playing, either prints player's typing speed or returns words per minute.
+    """
+    end_time = (time.time() / 60)
+    final_time = end_time - time_start
+    words_per_minute = (correct_words / final_time)
+    print(Fore.YELLOW + "GAME OVER! Counting words per minute..." + Style.RESET_ALL)
+    time.sleep(2)
+    clear_terminal()
+    if mode == 1:
+        print(Fore.YELLOW + "Your typing speed is: " + Fore.GREEN + str(round(words_per_minute, 1)) + Fore.YELLOW + " Words per minute" + Style.RESET_ALL)
+    elif mode == 2:
+        return round(words_per_minute, 1)
+
+
+def mode_selector(mode: int):
+
     """
     Selects game mode player selected in `start_menu()`.
 
     Parameters
     ----------
-    mode: int
+    - mode: int
 
     Modes
     ----------
     - Mode 1: Words per minute mode (Counts words per minute player typed, does not include mistakes)
     - Mode 2: Vs opponent mode (A race against a fictional opponent. Who has more WpM wins)
-    - Mode 3: Exit the application
+    - Mode 3: Exit the game
 
 
-    Any wrong input restarts the mode selector, allowing player to choose again
+    Any wrong input restarts the mode selector, allowing player to choose again.
     """
     if mode == 1:
         clear_terminal()
@@ -156,11 +179,12 @@ def mode_selector(mode : int):
         time.sleep(1)
         clear_terminal()
         sys.exit(0)
-    elif mode not in [1,2,3]:
+    elif mode not in [1, 2, 3]:
         print(Fore.RED + "Wrong input! Try again! Restarting in 3 seconds..." + Style.RESET_ALL)
         time.sleep(3)
         clear_terminal()
         start_menu()
+
 
 def start_menu():
     """
@@ -191,7 +215,8 @@ def start_menu():
         clear_terminal()
         start_menu()
 
-def gm(mode : int, opponent_wpm = 0):
+
+def gm(mode: int, opponent_wpm=0):
     """
     Runs a typing game session where the player types a sequence of words as fast and accurately as possible.
 
@@ -224,7 +249,7 @@ def gm(mode : int, opponent_wpm = 0):
     player_wpm = 0
     words_count = 25
     while run:
-        print("Type this word: " + Fore.LIGHTBLUE_EX + f"{words_to_play[index]}" +  Style.RESET_ALL + f" {index}/{words_count}")
+        print("Type this word: " + Fore.LIGHTBLUE_EX + f"{words_to_play[index]}" + Style.RESET_ALL + f" {index}/{words_count}")
         if input().upper() == words_to_play[index]:
             correct_words += 1
             index += 1
@@ -245,6 +270,6 @@ def gm(mode : int, opponent_wpm = 0):
         elif player_wpm == opponent_wpm:
             print(Fore.YELLOW + "Its a draw!" + Style.RESET_ALL)
 
+
 if __name__ == "__main__":
     start_menu()
-
